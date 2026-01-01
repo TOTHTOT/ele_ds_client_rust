d351238030d587abc113f9167e5872c002eea74e
TOTHTOT <mczyfs@gmail.com>
2026-01-01 17:51:22 +0800

    添加功能
    1. 添加ssd1680驱动, 现在能显示了.

--- src/board/mod.rs
+++ src/board/mod.rs
@@ -3,12 +3,20 @@ mod psram;
 use crate::communication::http_server::HttpServer;
 use crate::device_config::DeviceConfig;
 use crate::file_system::nvs_flash_filesystem_init;
+use embedded_graphics::prelude::*;
+use embedded_graphics::primitives::{Circle, PrimitiveStyle, Rectangle};
 use embedded_svc::wifi;
 use embedded_svc::wifi::AuthMethod;
 use esp_idf_svc::eventloop::EspSystemEventLoop;
+use esp_idf_svc::hal::delay::Ets;
+use esp_idf_svc::hal::gpio::{AnyIOPin, PinDriver};
 use esp_idf_svc::hal::peripherals::Peripherals;
+use esp_idf_svc::hal::spi;
+use esp_idf_svc::hal::spi::{SpiDeviceDriver, SpiDriver, SpiDriverConfig};
 use esp_idf_svc::nvs::{EspNvsPartition, NvsDefault};
 use esp_idf_svc::wifi::EspWifi;
+use ssd1680::color::{Black, Red};
+use ssd1680::prelude::{Display, DisplayAnyIn, DisplayRotation, Ssd1680};
 use std::str::FromStr;
 
 #[allow(dead_code)]
@@ -26,14 +34,42 @@ impl<'d> BoardPeripherals<'d> {
 
         let device_config = BoardPeripherals::init_filesystem_load_config()?;
 
-        /*let driver_config = Default::default();
-        let spi_drv = SpiDriver::new(
-            peripherals.spi2,
-            peripherals.pins.gpio12,
-            peripherals.pins.gpio11,
-            None::<Gpio0>,
-            &driver_config,
-        )?;*/
+        let spi = peripherals.spi2;
+
+        let sclk = peripherals.pins.gpio4;
+        let sdo = peripherals.pins.gpio5;
+        let rst = PinDriver::output(peripherals.pins.gpio6)?;
+        let dc = PinDriver::output(peripherals.pins.gpio7)?;
+        let cs = peripherals.pins.gpio15;
+        let busy = PinDriver::input(peripherals.pins.gpio16)?;
+
+        let spi = SpiDriver::new(
+            spi,
+            sclk,
+            sdo,
+            None::<AnyIOPin>,
+            &SpiDriverConfig::default(),
+        )?;
+
+        let mut spi = SpiDeviceDriver::new(spi, Some(cs), &spi::config::Config::new())?;
+
+        let mut delay = Ets;
+        let mut ssd1680 = Ssd1680::new(&mut spi, busy, dc, rst, &mut delay, 128, 296).unwrap();
+        ssd1680.clear_bw_frame().unwrap();
+        let mut display_bw = DisplayAnyIn::bw(128, 296);
+        display_bw.set_rotation(DisplayRotation::Rotate270);
+        Rectangle::new(Point::new(0, 20), Size::new(40, 40))
+            .into_styled(PrimitiveStyle::with_fill(Black))
+            .draw(&mut display_bw)
+            .unwrap();
+
+        Circle::new(Point::new(80, 80), 40)
+            .into_styled(PrimitiveStyle::with_fill(Black))
+            .draw(&mut display_bw)
+            .unwrap();
+        log::info!("Send bw frame to display");
+        ssd1680.update_bw_frame(display_bw.buffer()).unwrap();
+        ssd1680.display_frame(&mut delay).unwrap();
 
         let mut wifi = EspWifi::new(peripherals.modem, sysloop, Some(nvs.clone()))?;
         if let Some(ssid) = device_config.wifi_ssid.as_ref().take() {
